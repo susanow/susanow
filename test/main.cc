ef508f755982dc9ade8ddcc970b02121664824ed
slankdev <slank.dev@gmail.com>
2017-02-12 16:04:45 +0900

    WIP: add dta application

--- test/main.cc
+++ test/main.cc
@@ -16,66 +16,106 @@ using System = ssnlib::System_interface<Cpu, Port>;
 
 
 
+class dta2 {
 
+    class Cmd_dta : public ssnlib::Command {
+        struct pcmd_params {
+            cmdline_fixed_string_t cmd;
+            cmdline_fixed_string_t op;
+        };
+        dta2* d;
+    public:
+        Cmd_dta(dta2* d_) : d(d_)
+        {
+            append_token(TOKEN_STRING(struct pcmd_params, cmd, "dta"));
+            append_token(TOKEN_STRING(struct pcmd_params, op , NULL));
+            token_fin();
 
-class Cmd_drcp : public ssnlib::Command {
+            init_raw();
+            set_raw("dta < inc** | dec** | stop >");
+        }
+        void handle(void* p)
+        {
+            pcmd_params* param = reinterpret_cast<pcmd_params*>(p);
+            const std::string op = param->op;
+
+            if (op == "stop") d->stop();
+            else if (op == "incwk") d->inc(new Thrd_wk);
+            else if (op == "incrx") d->inc(new Thrd_rx);
+            else if (op == "decwk") d->dec("wk");
+            else if (op == "decrx") d->dec("rx");
+            else printf("Bad arguments\n");
+        }
+    };
 
     class Thrd_wk : public ssnlib::ssn_thread {
-        System* sys;
         bool running;
         size_t id;
     public:
-        Thrd_wk(System* s) : ssn_thread("drcp_thread"), sys(s), running(false)
-        {
-            static size_t cnt = 0;
-            id = cnt;
-            cnt++;
-        }
+        Thrd_wk() : ssn_thread("wk"), running(false) {}
         bool kill() { running = false; return true; }
         void operator()()
         {
             running = true;
             while (running) {
-                printf("%zd", id);
+                // printf("wk");
+                fflush(stdout);
+                sleep(1);
+            }
+        }
+    };
+    class Thrd_rx : public ssnlib::ssn_thread {
+        bool running;
+        size_t id;
+    public:
+        Thrd_rx() : ssn_thread("rx"), running(false) {}
+        bool kill() { running = false; return true; }
+        void operator()()
+        {
+            running = true;
+            while (running) {
+                // printf("rx");
                 fflush(stdout);
                 sleep(1);
             }
         }
     };
 
-private:
-    struct pcmd_params {
-        cmdline_fixed_string_t cmd;
-        cmdline_fixed_string_t op;
-    };
-    System* sys;
-    const std::string threadname;
 public:
-    Cmd_drcp(System* s) : sys(s), threadname("drcp_thread")
-    {
-        append_token(TOKEN_STRING(struct pcmd_params, cmd, "drcp"));
-        append_token(TOKEN_STRING(struct pcmd_params, op , NULL  ));
-        token_fin();
+    System sys;
+    ssnlib::Shell shell;
 
-        init_raw();
-        set_raw("drcp < inc | dec | start | stop >: drcp control");
+public:
+    dta2(int argc, char** argv) : sys(argc, argv), shell("susanow> ")
+    {
+        shell.add_cmd(new Cmd_clear           );
+        shell.add_cmd(new Cmd_findthread(&sys));
+        shell.add_cmd(new Cmd_quit      (&sys));
+        shell.add_cmd(new Cmd_thread    (&sys));
+        shell.add_cmd(new Cmd_port      (&sys));
+        shell.add_cmd(new Cmd_lscpu     (&sys));
+        shell.add_cmd(new Cmd_version   (&sys));
+        shell.add_cmd(new Cmd_dta       (this));
+        shell.fin();
+
+        sys.append_thread(&shell);
     }
-    void inc()
+    void inc(ssnlib::ssn_thread* thread)
     {
         try {
-            sys->append_thread(new Thrd_wk(sys));
+            sys.append_thread(thread);
             start();
         } catch (std::exception& e) {
             printf("can't append thread\n");
         }
     }
-    void dec()
+    void dec(const std::string& threadname)
     {
-        size_t nb_cpus = sys->cpus.size();
+        size_t nb_cpus = sys.cpus.size();
         for (ssize_t i=nb_cpus-1; i>=0; i--) {
             if (i == 0) continue;
 
-            Cpu& cpu = sys->cpus.at(i);
+            Cpu& cpu = sys.cpus.at(i);
             if (cpu.thread && cpu.thread->name == threadname) {
                 ssnlib::ssn_thread* tmp = cpu.thread;
                 if (cpu.get_state() != RUNNING) {
@@ -92,31 +132,20 @@ public:
     }
     void start()
     {
-        for (Cpu& cpu : sys->cpus) {
-            if (cpu.thread && cpu.thread->name==threadname) {
-                cpu.launch();
-            }
+        for (Cpu& cpu : sys.cpus) {
+            if (cpu.thread) cpu.launch();
         }
     }
     void stop()
     {
-        for (Cpu& cpu : sys->cpus) {
-            if (cpu.thread && cpu.thread->name==threadname) {
+        for (Cpu& cpu : sys.cpus) {
+            if (cpu.thread && cpu.thread->name == "shell") continue;
+
+            if (cpu.thread) {
                 cpu.thread->kill();
                 cpu.wait();
             }
         }
-    }
-    void handle(void* p)
-    {
-        pcmd_params* param = reinterpret_cast<pcmd_params*>(p);
-        std::string op = param->op;
-
-        if (op == "inc")        inc();
-        else if (op == "dec"  ) dec();
-        else if (op == "start") start();
-        else if (op == "stop" ) stop();
-        else printf("Bad Arguments \n");
     }
 };
 
@@ -131,22 +160,9 @@ int main(int argc, char** argv)
     Port::rx_ring_size   = 128;
     Port::tx_ring_size   = 512;
 
-    System sys(argc, argv);
-    if (sys.ports.size()%2 != 0) return -1;
+    dta2 d(argc, argv);
 
-    Shell shell("susanow> ");
-    shell.add_cmd(new Cmd_clear           );
-    shell.add_cmd(new Cmd_findthread(&sys));
-    shell.add_cmd(new Cmd_quit      (&sys));
-    shell.add_cmd(new Cmd_thread    (&sys));
-    shell.add_cmd(new Cmd_port      (&sys));
-    shell.add_cmd(new Cmd_lscpu     (&sys));
-    shell.add_cmd(new Cmd_version   (&sys));
-    shell.add_cmd(new Cmd_drcp      (&sys));
-    shell.fin();
-
-    sys.append_thread(&shell);
-    sys.launch_all();
-    sys.wait_all();
+    d.sys.launch_all();
+    d.sys.wait_all();
 }
 
