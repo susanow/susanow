980ff89792b45dd13d96aacb9e7a0aed4897f525
slank@mbp <slank.dev@gmail.com>
2017-03-12 15:18:23 +0900

    Refact

--- shell/commands.h
+++ shell/commands.h
@@ -1,11 +1,13 @@
 
 #pragma once
+#include <vty.h>
 
+namespace slankdev {
 
-struct Cmd_show : public node {
-    struct author : public node {
-        author() : node("author") {}
-        void function(shell* sh)
+struct Cmd_show : public vty::cmd_node {
+    struct author : public cmd_node {
+        author() : cmd_node("author") {}
+        void function(vty::shell* sh)
         {
             sh->Printf("Hiroki SHIROKURA.\r\n");
             sh->Printf(" Twitter : @slankdev\r\n");
@@ -14,36 +16,34 @@ struct Cmd_show : public node {
             sh->Printf(" E-mail  : slank.dev@gmail.com\r\n");
         }
     };
-    struct version : public node {
-        version() : node("version") {}
-        void function(shell* sh)
+    struct version : public cmd_node {
+        version() : cmd_node("version") {}
+        void function(vty::shell* sh)
         {
             sh->Printf("Susanow 0.0.0\r\n");
             sh->Printf("Copyright 2017-2020 Hiroki SHIROKURA.\r\n");
         }
     };
-    struct thread_info : public node {
-        thread_info() : node("thread-info") {}
-        void function(shell* sh)
+    struct thread_info : public cmd_node {
+        thread_info() : cmd_node("thread-info") {}
+        void function(vty::shell* sh)
         {
             sh->Printf("show thread-info\r\n");
         }
     };
-    Cmd_show() : node("show")
+    Cmd_show() : cmd_node("show")
     {
         commands.push_back(new author);
         commands.push_back(new version);
         commands.push_back(new thread_info);
     }
-    void function(shell* sh) { sh->Printf("show\r\n"); }
+    void function(vty::shell* sh) { sh->Printf("show\r\n"); }
 };
 
-struct Cmd_quit : public node {
-    Cmd_quit() : node("quit") {}
-    void function(shell* sh)
-    {
-        sh->Printf("quit system\r\n");
-        sh->closed = true;
-    }
+
+struct Cmd_quit : public vty::cmd_node {
+    Cmd_quit() : cmd_node("quit") {}
+    void function(vty::shell* sh) { sh->close(); }
 };
 
+}
--- shell/main.cc
+++ shell/main.cc
@@ -1,62 +1,14 @@
 
-#include "vty.h"
-#include "user_cmd.h"
-#include "user_kf.h"
-#include <slankdev/string.h>
-
-
-
-void keyfunction_init()
-{
-    using namespace slankdev;
-
-    uint8_t up   [] = {AC_ESC, '[', AC_A};
-    uint8_t down [] = {AC_ESC, '[', AC_B};
-    uint8_t right[] = {AC_ESC, '[', AC_C};
-    uint8_t left [] = {AC_ESC, '[', AC_D};
-    vty::add_keyfunction(new KF_hist_search_deep   (up   , sizeof(up   )));
-    vty::add_keyfunction(new KF_hist_search_shallow(down , sizeof(down )));
-    vty::add_keyfunction(new KF_right(right, sizeof(right)));
-    vty::add_keyfunction(new KF_left (left , sizeof(left )));
-
-    uint8_t ctrlP[] = {AC_Ctrl_P};
-    uint8_t ctrlN[] = {AC_Ctrl_N};
-    uint8_t ctrlF[] = {AC_Ctrl_F};
-    uint8_t ctrlB[] = {AC_Ctrl_B};
-    vty::add_keyfunction(new KF_hist_search_deep   (ctrlP, sizeof(ctrlP)));
-    vty::add_keyfunction(new KF_hist_search_shallow(ctrlN, sizeof(ctrlN)));
-    vty::add_keyfunction(new KF_right(ctrlF, sizeof(ctrlF)));
-    vty::add_keyfunction(new KF_left (ctrlB, sizeof(ctrlB)));
-
-    uint8_t d1[] = {'?'};
-    uint8_t d5[] = {'\t'};
-    vty::add_keyfunction(new KF_question(d1, sizeof(d1)));
-    vty::add_keyfunction(new KF_question(d5, sizeof(d5)));
-
-    uint8_t d2[] = {'\r', '\0'};
-    vty::add_keyfunction(new KF_return  (d2, sizeof(d2)));
-
-    uint8_t d3[] = {0x7f};
-    vty::add_keyfunction(new KF_delete  (d3, sizeof(d3)));
-}
-
-
-void command_init()
-{
-    vty::add_command(new Cmd_show);
-    vty::add_command(new Cmd_quit);
-}
+#include <vty.h>
+#include "commands.h"
 
 
 int main()
 {
-    keyfunction_init();
-    command_init();
-    vty::set_port(9999);
+    slankdev::vty vty0(9999);
 
-    vty vty;
-    vty.dispatch();
+    vty0.add_command(new slankdev::Cmd_show);
+    vty0.add_command(new slankdev::Cmd_quit);
+    vty0.dispatch();
 }
-
-
 
