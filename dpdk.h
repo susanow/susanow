8228b0c4a92905e2a6a9c93f7f3bff1ed984be8f
slankdev <slank.dev@gmail.com>
2017-03-12 22:21:54 +0900

    Add: dpdk.h

---
+++ dpdk.h
@@ -0,0 +1,16 @@
+
+#pragma once
+
+#include <rte_ring.h>
+#include <rte_config.h>
+#include <rte_version.h>
+#include <rte_eal.h>
+#include <rte_ethdev.h>
+#include <rte_ether.h>
+#include <rte_cycles.h>
+#include <rte_lcore.h>
+#include <rte_mbuf.h>
+#include <rte_hexdump.h>
+#include <rte_ip.h>
+#include <rte_ip_frag.h>
+
