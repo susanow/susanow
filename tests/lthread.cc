ec43d0fa9cfdca74ac2eb535a80a541d9a89c4d4
Hiroki SHIROKURA <slank.dev@gmail.com>
2017-06-21 16:43:00 +0900

    WIP ssn_lthread: fix memory leak

--- tests/lthread.cc
+++ tests/lthread.cc
@@ -10,12 +10,15 @@ size_t num3=3;
 size_t num4=4;
 size_t num5=5;
 
-void test(void*)
+inline void _sleep() { printf("."); fflush(stdout); sleep(1); }
+
+void test(void* arg)
 {
+  size_t* n = (size_t*)arg;
   printf("test: start\n");
   for (size_t i=0; i<3; i++) {
-    printf("test\n");
-    lthread_sleep(1000);
+    printf("test arg=%zd \n", *n);
+    ssn_sleep(1000);
   }
   printf("test: fin\n");
 }
@@ -25,8 +28,9 @@ int main(int argc, char** argv)
   ssn_init(argc, argv);
 
   ssn_lthread_sched_register(1);
-  ssn_lthread_launch(test, nullptr, 1);
-  sleep(5);
+  // ssn_lthread_launch(test, &num0, 1);
+  // ssn_lthread_launch(test, &num1, 1);
+  _sleep();
   ssn_lthread_sched_unregister(1);
 
   ssn_fin();
