40e742241de49466b65886433e5e785bf378fbbc
Hiroki SHIROKURA <slank.dev@gmail.com>
2017-06-22 16:49:48 +0900

    Add: lthread_dpdk

--- mk/ssn.vars.mk
+++ mk/ssn.vars.mk
@@ -1,13 +1,13 @@
 
 
 DPDK_PATH        = $(SSN_SDK)/dpdk/x86_64-native-linuxapp-gcc
-LTHREAD_PATH     = $(SSN_SDK)/lthread
+LTHREAD_PATH     = $(SSN_SDK)/lthread_dpdk
 LIBSLANKDEV_PATH = $(SSN_SDK)/libslankdev
 LIBSSN_PATH      = $(SSN_SDK)/lib
 
 SSN_LIB_CXXFLAGS += \
 		-I$(DPDK_PATH)/include \
-		-I$(LTHREAD_PATH)/src  \
+		-I$(LTHREAD_PATH)      \
 		-I$(LIBSLANKDEV_PATH)  \
 		-I$(LIBSSN_PATH)       \
 		-m64 -mssse3           \
@@ -18,8 +18,8 @@ SSN_LIB_LDFLAGS  += \
 	  -L$(LTHREAD_PATH)          \
 	  -L$(LIBSLANKDEV_PATH)      \
 		-L$(LIBSSN_PATH)           \
-		-ldpdk -lpthread -ldl -lrt \
-		-llthread
+		-llthread_dpdk \
+		-ldpdk -lpthread -ldl -lrt
 
 SSN_CXXFLAGS =  $(SSN_LIB_CXXFLAGS)
 SSN_LDFLAGS  = -lsusanow $(SSN_LIB_LDFLAGS)
