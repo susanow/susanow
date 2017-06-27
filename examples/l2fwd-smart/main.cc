96637a8de58903996899ee25a704084c756ff35a
Hiroki SHIROKURA <slank.dev@gmail.com>
2017-06-27 23:18:56 +0900

    WIP example: l2fwd-smart

--- examples/l2fwd-smart/main.cc
+++ examples/l2fwd-smart/main.cc
@@ -5,27 +5,92 @@
 #include <susanow.h>
 #include <slankdev/extra/dpdk.h>
 
+size_t num0 = 0;
+size_t num1 = 1;
+size_t num2 = 2;
+size_t num3 = 3;
+size_t num4 = 4;
 
-bool running;
-void l2fwd(void*)
+rte_ring* pre_tx[2];
+rte_ring* post_rx[2];
+
+bool running = true;
+void rx_q(void* arg)
+{
+  size_t qid = *reinterpret_cast<size_t*>(arg);
+  size_t nb_ports = ssn_dev_count();
+  printf("start rx-thread nb_ports=%zd \n", nb_ports);
+  while (running) {
+    for (size_t pid=0; pid<nb_ports; pid++) {
+      rte_mbuf* mbufs[32];
+      size_t nb_recv = ssn_port_rx_burst(pid, qid, mbufs, 32);
+      if (nb_recv == 0) continue;
+
+      size_t nb_enq = rte_ring_enqueue_bulk(post_rx[pid], (void**)mbufs, nb_recv, nullptr);
+      if (nb_enq < nb_recv)
+        ssn_mbuf_free_bulk(&mbufs[nb_enq], nb_recv-nb_enq);
+    }
+  }
+}
+void rx(void*)
 {
   size_t nb_ports = ssn_dev_count();
-  printf("start l2fwd nb_ports=%zd \n", nb_ports);
-  running = true;
+  printf("start rx-thread nb_ports=%zd \n", nb_ports);
   while (running) {
     for (size_t pid=0; pid<nb_ports; pid++) {
       rte_mbuf* mbufs[32];
       size_t nb_recv = ssn_port_rx_burst(pid, 0, mbufs, 32);
       if (nb_recv == 0) continue;
 
-      size_t nb_send = ssn_port_tx_burst(pid^1, 0, mbufs, nb_recv);
-      if (nb_recv > nb_send) {
-        ssn_mbuf_free_bulk(&mbufs[nb_send], nb_recv-nb_send);
-      }
+      size_t nb_enq = rte_ring_enqueue_bulk(post_rx[pid], (void**)mbufs, nb_recv, nullptr);
+      if (nb_enq < nb_recv)
+        ssn_mbuf_free_bulk(&mbufs[nb_enq], nb_recv-nb_enq);
     }
-    ssn_yield();
   }
 }
+void wk(void*)
+{
+  static size_t count = 0;
+  count ++;
+  size_t nb_ports = ssn_dev_count();
+  printf("start wk-thread nb_ports=%zd nb_wks=%zd \n", nb_ports, count);
+  while (running) {
+    for (size_t pid=0; pid<nb_ports; pid++) {
+#if 1
+      rte_mbuf* mbufs[32];
+      size_t nb_deq = rte_ring_dequeue_bulk(post_rx[pid], (void**)mbufs, 32, nullptr);
+      if (nb_deq == 0) continue;
+      rte_delay_us_block(10);
+      size_t nb_enq = rte_ring_enqueue_bulk(pre_tx[pid^1], (void**)mbufs, nb_deq, nullptr);
+      if (nb_enq < nb_deq)
+        ssn_mbuf_free_bulk(&mbufs[nb_enq], nb_deq-nb_enq);
+#else
+      rte_mbuf* msg;
+      int ret = rte_ring_dequeue(post_rx[pid], (void**)&msg);
+      if (ret < 0) continue;
+      rte_delay_us_block(1);
+      ret = rte_ring_enqueue(pre_tx[pid^1], msg);
+      if (ret < 0) rte_pktmbuf_free(msg);
+#endif
+    }
+  }
+}
+void tx(void*)
+{
+  size_t nb_ports = ssn_dev_count();
+  printf("start tx-thread nb_ports=%zd \n", nb_ports);
+  while (running) {
+    for (size_t pid=0; pid<nb_ports; pid++) {
+      rte_mbuf* mbufs[32];
+      size_t nb_deq = rte_ring_dequeue_bulk(pre_tx[pid], (void**)mbufs, 32, nullptr);
+      size_t nb_send = ssn_port_tx_burst(pid, 0, mbufs, nb_deq);
+      if (nb_deq > nb_send) {
+        ssn_mbuf_free_bulk(&mbufs[nb_send], nb_deq-nb_send);
+      }
+    }
+  }
+}
+
 
 void wait_enter(const char* msg)
 {
@@ -104,16 +169,9 @@ void get_dev_stat(void* arg)
   stats->dump();
 }
 
-void test(void*)
-{
-  while (running) {
-    printf("faaa\n");
-    ssn_sleep(1000);
-  }
-}
-
 int main(int argc, char** argv)
 {
+  ssn_log_set_level(SSN_LOG_DEBUG);
   ssn_init(argc, argv);
   ssn_timer_sched_register(1);
 
@@ -124,6 +182,7 @@ int main(int argc, char** argv)
   ssn_timer_add(tim, 1);
 
   ssn_port_conf conf;
+  conf.nb_rxq = 2;
   for (size_t i=0; i<nb_ports; i++) {
     ssn_port_configure(i, &conf);
     ssn_port_dev_up(i);
@@ -131,13 +190,32 @@ int main(int argc, char** argv)
     ssn_port_promisc_on(i);
   }
 
-  ssn_lthread_launch(l2fwd, nullptr, 2);
-  wait_enter("final");
+  post_rx[0] = slankdev::ring_alloc("post_rx[0]", 1024);
+  post_rx[1] = slankdev::ring_alloc("post_rx[1]", 1024);
+  pre_tx[0]  = slankdev::ring_alloc("pre_tx[0] ", 1024);
+  pre_tx[1]  = slankdev::ring_alloc("pre_tx[1] ", 1024);
+
+  ssn_native_thread_launch(rx_q, &num0, 2);
+  ssn_native_thread_launch(rx_q, &num1, 7);
+  ssn_native_thread_launch(tx, nullptr, 3);
+  wait_enter("next");
+  ssn_native_thread_launch(wk, nullptr, 4);
+  wait_enter("next");
+  ssn_native_thread_launch(wk, nullptr, 5);
+  wait_enter("next");
+  ssn_native_thread_launch(wk, nullptr, 6);
+
+  wait_enter("Type Enter to Exit L2fwd");
   running = false;
-  ssn_sleep(1000);
 
   ssn_timer_sched_unregister(1);
   ssn_fin();
+
+  rte_ring_free(post_rx[0]);
+  rte_ring_free(post_rx[1]);
+  rte_ring_free(pre_tx[0] );
+  rte_ring_free(pre_tx[1] );
+
 }
 
 
