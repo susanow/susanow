1f7e17a62e5760d61ee8fb067c03ab2e804e79c9
Hiroki SHIROKURA <slank.dev@gmail.com>
2017-07-07 14:45:28 +0900

    Update: libslankdev

--- tests/rest.cc
+++ tests/rest.cc
@@ -1,56 +1,37 @@
 
 
-#include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <thread>
-#include <slankdev/filefd.h>
-#include "ssn_webui.h"
-#define PATH ""
+#include <slankdev/util.h>
+#include <slankdev/rest.h>
 
-bool running = true;
-void update(void* arg)
+
+void callback_root(int fd, const void* buf, size_t len, void*)
+{ slankdev::fdprintf(fd, "{ \"kanailab\" : \"Wonderful OBAKA Laboratory\" }\n"); }
+
+void callback_slankdev(int fd, const void* buf, size_t len, void*)
+{ slankdev::fdprintf(fd, "{ \"slankdev\" : \"Hiroki SHIROKURA\" }\n"); }
+
+void callback_yukaribonk(int fd, const void* buf, size_t len, void*)
+{ slankdev::fdprintf(fd, "{ \"yukaribonk\" : \"Yukari KUBO\" }\n"); }
+
+void callback_stats(int fd, const void* buf, size_t len, void*)
 {
-  ssn_stats* s = reinterpret_cast<ssn_stats*>(arg);
-  while (running) {
-    slankdev::filefd file;
-    file.fopen(PATH, "w+");
-    s->dump(file.getfp());
-    usleep(1000);
-  }
+  slankdev::fdprintf(fd,
+      "{"
+        "\"nb_vnfs\" : 3,"
+        "\"nb_nics\" : 4,"
+        "\"nb_thds\" : 5 "
+      "}"
+      );
 }
-void control(void* arg)
-{
-  ssn_stats* s = reinterpret_cast<ssn_stats*>(arg);
-  while (running) {
-    char c = getchar();
-    if (c == 'q') break;
-    s->nb_threads += 1;
-    s->dump(stdout);
-  }
-  running = false;
-}
+
 int main(int argc, char** argv)
 {
-  ssn_stats* s = new ssn_stats;
-  s->nb_threads = 1;
-  s->nb_cpus    = 2;
-  s->nb_ports   = 3;
-  s->author     = std::string("Hiroki SHIROKURA");
-  s->vnfs.resize(2);
-  s->vnfs[0].id = 123;
-  s->vnfs[0].name = std::string("slankdev");
-  s->vnfs[0].stages = 3;
-  s->vnfs[1].id = 456;
-  s->vnfs[1].name = std::string("yukaribonk");
-  s->vnfs[1].stages = 5;
-  s->dump(stdout);
-
-  std::thread t0(update , s);
-  std::thread t1(control, s);
-  t0.join();
-  t1.join();
+  slankdev::rest_server serv(INADDR_ANY, 80);
+  serv.add_route("/", callback_root);
+  serv.add_route("/slankdev", callback_slankdev);
+  serv.add_route("/yukaribonk", callback_yukaribonk);
+  serv.add_route("/stats" , callback_stats);
+  serv.dispatch(nullptr);
 }
-
 
