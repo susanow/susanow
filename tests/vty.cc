cb956f999e4ed23be95cec64b09c2cda4bf1f412
Hiroki SHIROKURA <slank.dev@gmail.com>
2017-07-07 17:48:42 +0900

    WIP: Enhance snn_vty

---
+++ tests/vty.cc
@@ -0,0 +1,18 @@
+
+#include <susanow.h>
+#include <slankdev/util.h>
+#include <slankdev/socketfd.h>
+
+int main(int argc, char** argv)
+{
+  ssn_init(argc, argv);
+
+  ssn_vty vty(INADDR_ANY, 8888);
+
+  ssn_native_thread_launch(ssn_rest_poll_thread, &vty, 1);
+  getchar();
+  ssn_vty_poll_thread_stop();
+
+  ssn_wait_all_lcore();
+  ssn_fin();
+}
