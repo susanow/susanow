744ba351a4c533063ae75cfd4fcbf04c09227e19
Hiroki SHIROKURA <slank.dev@gmail.com>
2017-07-24 21:24:17 +0900

    ADD: ssn_vnf

--- examples/vnf/vnf.h
+++ examples/vnf/vnf.h
@@ -56,7 +56,8 @@ class func_wk : public func {
       for (size_t p=0; p<nb_ports; p++) {
         size_t deqlen = prewk_[p]->deq_bulk((void**)mbufs, 32);
         for (size_t i=0; i<deqlen; i++) {
-          for (size_t j=0; j<100; j++) ; // DELAY
+          // for (size_t j=0; j<100; j++) ; // DELAY
+          for (size_t j=0; j<30; j++) ; // DELAY
           int ret = poswk_[p^1]->enq(mbufs[i]);
           if (ret < 0) rte_pktmbuf_free(mbufs[i]);
         }
@@ -111,10 +112,13 @@ class func_tx : public func {
 };
 
 class stage {
+ protected:
   size_t mux_;
  public:
+  const std::string name;
   std::vector<func*> funcs;
-  stage() : mux_(0) {}
+  stage(const char* n) : name(n), mux_(0) {}
+  virtual size_t throughput_pps() const = 0;
   virtual ~stage() {}
   virtual func* allocate() = 0;
   virtual void inc()
@@ -139,6 +143,7 @@ class stage {
 class stage_rx : public stage {
  public:
   ssn_ring* prewk_[2];
+  stage_rx(const char* n) : stage(n) {}
   virtual func* allocate() override
   {
     func_rx* rx = new func_rx;
@@ -146,10 +151,21 @@ class stage_rx : public stage {
     rx->prewk_[1] = prewk_[1];
     return rx;
   }
+  virtual size_t throughput_pps() const override
+  {
+    size_t sum_rx_pps = 0;
+    size_t nb_ports = ssn_dev_count();
+    for (size_t i=0; i<nb_ports; i++) {
+      auto pps = ssn_port_stat_get_cur_rx_pps(i);
+      sum_rx_pps += pps;
+    }
+    return sum_rx_pps;
+  }
 };
 
 class stage_wk : public stage {
  public:
+  stage_wk(const char* n) : stage(n) {}
   ssn_ring* prewk_[2];
   ssn_ring* poswk_[2];
   virtual func* allocate() override
@@ -161,10 +177,20 @@ class stage_wk : public stage {
     wk->poswk_[1] = poswk_[1];
     return wk;
   }
+  virtual size_t throughput_pps() const override
+  {
+    size_t sum_pps = 0;
+    size_t nb_ports = ssn_dev_count();
+    for (size_t i=0; i<nb_ports; i++) {
+      sum_pps += prewk_[i]->opps;
+    }
+    return sum_pps;
+  }
 };
 
 class stage_tx : public stage {
  public:
+  stage_tx(const char* n) : stage(n) {}
   ssn_ring* poswk_[2];
   virtual func* allocate() override
   {
@@ -173,6 +199,15 @@ class stage_tx : public stage {
     tx->poswk_[1] = poswk_[1];
     return tx;
   }
+  virtual size_t throughput_pps() const override
+  {
+    size_t sum_pps = 0;
+    size_t nb_ports = ssn_dev_count();
+    for (size_t i=0; i<nb_ports; i++) {
+      sum_pps += poswk_[i]->opps;
+    }
+    return sum_pps;
+  }
 };
 
 class vnf_l2fwd {
@@ -180,17 +215,17 @@ class vnf_l2fwd {
   std::vector<stage*> stages;
   vnf_l2fwd(ssn_ring** prewk, ssn_ring** poswk)
   {
-    stage_rx* rx = new stage_rx;
+    stage_rx* rx = new stage_rx("rx");
     rx->prewk_[0] = prewk[0];
     rx->prewk_[1] = prewk[1];
 
-    stage_wk* wk = new stage_wk;
+    stage_wk* wk = new stage_wk("wk");
     wk->prewk_[0] = prewk[0];
     wk->prewk_[1] = prewk[1];
     wk->poswk_[0] = poswk[0];
     wk->poswk_[1] = poswk[1];
 
-    stage_tx* tx = new stage_tx;
+    stage_tx* tx = new stage_tx("tx");
     tx->poswk_[0] = poswk[0];
     tx->poswk_[1] = poswk[1];
 
@@ -209,11 +244,25 @@ class vnf_l2fwd {
   {
     fprintf(fp, "vnf1 \r\n");
     size_t nb_stages = stages.size();
+
+    size_t stage_perf[nb_stages];
+    for (size_t i=0; i<nb_stages; i++) {
+      stage_perf[i] = stages[i]->throughput_pps();
+    }
+
     for (size_t i=0; i<nb_stages; i++) {
       void* ptr  = stages[i];
       size_t mux = stages[i]->mux();
-      fprintf(fp ," pl[%zd]: ptr=%p, mux=%zd \r\n",
-          i, stages[i], stages[i]->mux());
+      double r = 1.0;
+      if (i!=0) r = double(stage_perf[i])/double(stage_perf[i-1]);
+      fprintf(fp ," pl[%zd]: name=\"%s\" mux=%-2zd TP=%-10zd red=%lf",
+          i, stages[i]->name.c_str(),
+          stages[i]->mux(),
+          stages[i]->throughput_pps(), r);
+      if (r < 0.7) {
+        fprintf(fp, " <-- BOTTLE NECK");
+      }
+      fprintf(fp, "\r\n");
     }
   }
 };
