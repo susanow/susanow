9d793838056fe0f0279140d602ef8fb60f15f5b9
Hiroki SHIROKURA <slank.dev@gmail.com>
2017-07-25 01:00:30 +0900

    WIP: add stage class

---
+++ examples/vnf/stage.cc
@@ -0,0 +1,57 @@
+
+
+#include "stage.h"
+
+#include <string>
+#include <vector>
+#include <stdio.h>
+
+#include <slankdev/util.h>
+#include <slankdev/exception.h>
+#include <slankdev/extra/dpdk.h>
+
+#include <ssn_common.h>
+#include <ssn_sys.h>
+#include <ssn_timer.h>
+#include <ssn_native_thread.h>
+#include <ssn_green_thread.h>
+#include <ssn_port_stat.h>
+#include <ssn_log.h>
+#include <ssn_ring.h>
+#include <ssn_port.h>
+
+
+
+static inline void _func_spawner(void* arg)
+{
+  func* f = reinterpret_cast<func*>(arg);
+  f->poll_exe();
+}
+
+static inline ssize_t get_free_lcore_id()
+{
+  size_t nb_lcores = ssn_lcore_count();
+  for (size_t i=1; i<nb_lcores; i++) {
+    auto s = ssn_get_lcore_state(i);
+    if (s == SSN_LS_WAIT) return i;
+  }
+  return -1;
+}
+
+void stage::inc()
+{
+  func* f = allocate();
+  funcs.push_back(f);
+  ssize_t lcore_id = get_free_lcore_id();
+  if (lcore_id < 0) throw slankdev::exception("no lcore");
+  ssn_native_thread_launch(_func_spawner, f, lcore_id);
+  mux_ ++ ;
+}
+void stage::dec()
+{
+  size_t idx = funcs.size()-1;
+  funcs[idx]->stop();
+  funcs.erase(funcs.begin() + idx);
+  mux_ -- ;
+}
+size_t stage::mux() const { return mux_; }
---
+++ examples/vnf/stageio.h
@@ -0,0 +1,62 @@
+
+#pragma once
+
+#include <vector>
+#include <string>
+#include <stdint.h>
+#include <stddef.h>
+#include <slankdev/extra/dpdk.h>
+#include <ssn_ring.h>
+#include <ssn_port.h>
+#include <ssn_port_stat.h>
+
+
+class stageio_rx {
+ public:
+  stageio_rx() {}
+  virtual ~stageio_rx() {}
+  virtual size_t rx_burst(rte_mbuf** obj_table, size_t n) = 0;
+  virtual size_t rx_pps() const = 0;
+};
+class stageio_tx {
+ public:
+  stageio_tx() {}
+  virtual ~stageio_tx() {}
+  virtual int tx_shot(rte_mbuf* obj) = 0;
+  virtual size_t tx_burst(rte_mbuf** obj_table, size_t n) = 0;
+};
+
+class stageio_rx_ring : public stageio_rx {
+ public:
+  ssn_ring* ring;
+  virtual size_t rx_burst(rte_mbuf** obj_table, size_t n) override
+  { return ring->deq_bulk((void**)obj_table, n); }
+  virtual size_t rx_pps() const override { return ring->opps; }
+};
+class stageio_tx_ring : public stageio_tx {
+ public:
+  ssn_ring* ring;
+  virtual int tx_shot(rte_mbuf* obj) override { return ring->enq((void*)obj); }
+  virtual size_t tx_burst(rte_mbuf** obj_table, size_t n) override
+  { return ring->enq_bulk((void* const*)obj_table, n); }
+};
+
+class stageio_rx_port : public stageio_rx {
+ public:
+  size_t pid;
+  size_t qid;
+  virtual size_t rx_burst(rte_mbuf** obj_table, size_t n) override
+  { return rte_eth_rx_burst(pid, qid, obj_table, n); }
+  virtual size_t rx_pps() const override { return  ssn_port_stat_get_cur_rx_pps(pid); }
+};
+class stageio_tx_port : public stageio_tx {
+ public:
+  size_t pid;
+  size_t qid;
+  virtual int tx_shot(rte_mbuf* obj) override
+  { throw slankdev::exception("not imple"); }
+  virtual size_t tx_burst(rte_mbuf** obj_table, size_t n) override
+  { return rte_eth_tx_burst(pid, qid, obj_table, n); }
+};
+
+
