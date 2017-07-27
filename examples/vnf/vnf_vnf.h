d23fb77a8c3b554c3a8d1be2691e9f3b3889d543
Hiroki SHIROKURA <slank.dev@gmail.com>
2017-07-27 16:47:17 +0900

    WIP

--- examples/vnf/vnf_vnf.h
+++ examples/vnf/vnf_vnf.h
@@ -1,6 +1,83 @@
 
 #pragma once
+#include "func.h"
 
+
+class func_rx : public func {
+ public:
+
+  func_rx(std::vector<stageio_rx*>& _rx,
+          std::vector<stageio_tx*>& _tx) : func(_rx, _tx) {}
+  virtual void poll_exe() override
+  {
+    size_t nb_ports = ssn_dev_count();
+    run = true;
+    while (run) {
+      for (size_t p=0; p<nb_ports; p++) {
+        rte_mbuf* mbufs[32];
+        size_t recvlen = rx[p]->rx_burst(mbufs, 32);
+        if (recvlen == 0) continue;
+        size_t enqlen = tx[p]->tx_burst(mbufs, recvlen);
+        if (recvlen > enqlen) {
+          slankdev::rte_pktmbuf_free_bulk(&mbufs[enqlen], recvlen-enqlen);
+        }
+      }
+    }
+  }
+  virtual void stop() override { run = false; }
+};
+
+
+
+class func_wk : public func {
+ public:
+
+  func_wk(std::vector<stageio_rx*>& _rx,
+          std::vector<stageio_tx*>& _tx) : func(_rx, _tx) {}
+  virtual void poll_exe() override
+  {
+    ssn_log(SSN_LOG_INFO, "func_wk: INCLUDE DELAY\r\n");
+    size_t nb_ports = ssn_dev_count();
+    rte_mbuf* mbufs[32];
+    while (run) {
+      for (size_t p=0; p<nb_ports; p++) {
+        size_t deqlen = rx[p]->rx_burst(mbufs, 32);
+        for (size_t i=0; i<deqlen; i++) {
+          // for (size_t j=0; j<100; j++) ; // DELAY
+          for (size_t j=0; j<40; j++) ; // DELAY
+          int ret = tx[p^1]->tx_shot(mbufs[i]);
+          if (ret < 0) rte_pktmbuf_free(mbufs[i]);
+        }
+      }
+    }
+  }
+  virtual void stop() override { run = false; }
+};
+
+
+
+class func_tx : public func {
+ public:
+
+  func_tx(std::vector<stageio_rx*>& _rx,
+          std::vector<stageio_tx*>& _tx) : func(_rx, _tx) {}
+  virtual void poll_exe() override
+  {
+    size_t nb_ports = ssn_dev_count();
+    while (run) {
+      for (size_t p=0; p<nb_ports; p++) {
+        rte_mbuf* mbufs[32];
+        size_t deqlen = rx[p]->rx_burst(mbufs, 32);
+        if (deqlen == 0) continue;
+        size_t sendlen = tx[p]->tx_burst(mbufs, deqlen);
+        if (deqlen > sendlen) {
+          slankdev::rte_pktmbuf_free_bulk(&mbufs[sendlen], deqlen-sendlen);
+        }
+      }
+    }
+  }
+  virtual void stop() override { run = false; }
+};
 
 func* alloc_func_rx(void* arg)
 {
@@ -47,10 +124,10 @@ class vnf_l2fwd : public vnf {
     stages.push_back(wk);
 
     stage* tx = new stage("tx", alloc_func_tx);
-    wk->add_input_ring(ring_poswk[0]);
-    wk->add_input_ring(ring_poswk[1]);
-    wk->add_output_port(0);
-    wk->add_output_port(1);
+    tx->add_input_ring(ring_poswk[0]);
+    tx->add_input_ring(ring_poswk[1]);
+    tx->add_output_port(0);
+    tx->add_output_port(1);
     stages.push_back(tx);
   }
 };
