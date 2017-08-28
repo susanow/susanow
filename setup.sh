c7923076f977589870c0453b945fc2b7de2f49d1
Hiroki SHIROKURA <slank.dev@gmail.com>
2017-08-28 06:42:10 +0000

    Update: setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,46 @@
-#!/bin/sh
-git submodule init
-git submodule update
+#!/bin/bash
 
-sudo apt install -y libpcap-dev
-sudo apt install -y linux-headers-`uname -r`
+function install_packages() {
+	git submodule init
+	git submodule update
+	sudo apt install -y libpcap-dev
+	sudo apt install -y linux-headers-`uname -r`
+}
 
-echo "Build DPDK"
-cd dpdk
-export RTE_SDK=`pwd`
-export RTE_TARGET=x86_64-native-linuxapp-gcc
-make install T=$RTE_TARGET
-cd ..
+function build_dpdk() {
+	echo "Build DPDK"
+	cd dpdk
+	export RTE_SDK=`pwd`
+	export RTE_TARGET=x86_64-native-linuxapp-gcc
+	make install T=$RTE_TARGET
+	if [ ! $? = "0" ]; then
+		exit -1;
+	fi
+	cd ..
+}
 
-make -C lib/liblthread_dpdk
-make -C lib/libvty
-make -C lib/libsusanow
-make -C tests build
+function build_libs() {
+	make -C lib/liblthread_dpdk
+	if [ ! $? = "0" ]; then
+		exit -1
+	fi
+	make -C lib/libvty
+	if [ ! $? = "0" ]; then
+		exit -1
+	fi
+	make -C lib/libsusanow
+	if [ ! $? = "0" ]; then
+		exit -1
+	fi
+	make -C tests build
+	if [ ! $? = "0" ]; then
+		exit -1
+	fi
+}
 
+
+install_packages
+build_dpdk
+build_libs
 echo "Success Setup for SUSANOW"
+
