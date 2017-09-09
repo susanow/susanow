7d37ef634746c7a4f3da1cd24fa3494905f25bdb
Hiroki SHIROKURA <slank.dev@gmail.com>
2017-09-10 06:20:58 +0900

    Refact ssn_vnf

--- examples/ssn_vnf/main.cc
+++ examples/ssn_vnf/main.cc
@@ -6,7 +6,6 @@
 #include <ssn_log.h>
 #include <ssn_cpu.h>
 #include <ssn_port_stat.h>
-
 #include <memory>
 #include <stdio.h>
 #include <unistd.h>
@@ -22,69 +21,72 @@ size_t num5 = 5;
 size_t num6 = 6;
 size_t num7 = 7;
 size_t num8 = 8;
-size_t nb_rxq = 4;
-size_t nb_txq = 4;
-
-struct que {
-  size_t pid;
-  size_t qid;
-};
+bool block = false;
 
 struct port {
+  size_t rxques_idx;
+  size_t txques_idx;
+
   size_t pid;
   std::vector<size_t> ques;
-  size_t rx_burst(rte_mbuf* mbufs, size_t nb_bursts);
-  size_t tx_burst(rte_mbuf* mbufs, size_t nb_bursts);
+
+  port() : rxques_idx(0), txques_idx(0) {}
+  size_t rx_burst(rte_mbuf** mbufs, size_t nb_bursts);
+  size_t tx_burst(rte_mbuf** mbufs, size_t nb_bursts);
 };
 
-size_t port::rx_burst(rte_mbuf* mbufs, size_t nb_bursts)
+size_t port::rx_burst(rte_mbuf** mbufs, size_t nb_bursts)
 {
-  return 0;
+  rxques_idx = (rxques_idx + 1) % ques.size();
+  size_t nb_recv = ssn_port_rx_burst(pid, ques.at(rxques_idx), mbufs, nb_bursts);
+  return nb_recv;
 }
-size_t port::tx_burst(rte_mbuf* mbufs, size_t nb_bursts)
+size_t port::tx_burst(rte_mbuf** mbufs, size_t nb_bursts)
 {
-  return 0;
+  txques_idx = (txques_idx + 1) % ques.size();
+  size_t nb_send = ssn_port_tx_burst(pid, ques.at(txques_idx), mbufs, nb_bursts);
+  return nb_send;
 }
 
 
 class thread_conf final {
  public:
   size_t lcore_id;
-  std::vector<port> rx_port_range;
-  std::vector<port> tx_port_range;
+  std::vector<port> ports;
 
   virtual ~thread_conf() {}
   void clear()
   {
     lcore_id = 0;
-    rx_port_range.clear();
-    tx_port_range.clear();
+    ports.clear();
   }
   void dump(FILE* fp) const
   {
     fprintf(fp, "  lcore_id=%zd \n", lcore_id);
 
-    fprintf(fp, "  rx_por_range={\n");
-    for (size_t pid=0; pid<rx_port_range.size(); pid++) {
-      fprintf(fp, "    port%zd\n", rx_port_range[pid].pid);
-      for (size_t qid=0; qid<rx_port_range[pid].ques.size(); qid++) {
-        fprintf(fp, "      queue%zd\n", rx_port_range[pid].ques[qid]);
-      }
-    }
-    fprintf(fp, "  }\n");
-
-    fprintf(fp, "  tx_por_range={\n");
-    for (size_t pid=0; pid<tx_port_range.size(); pid++) {
-      fprintf(fp, "    port%zd\n", tx_port_range[pid].pid);
-      for (size_t qid=0; qid<tx_port_range[pid].ques.size(); qid++) {
-        fprintf(fp, "      queue%zd\n", tx_port_range[pid].ques[qid]);
+    fprintf(fp, "  por_range={\n");
+    for (size_t pid=0; pid<ports.size(); pid++) {
+      fprintf(fp, "    port%zd\n", ports[pid].pid);
+      for (size_t qid=0; qid<ports[pid].ques.size(); qid++) {
+        fprintf(fp, "      queue%zd\n", ports[pid].ques[qid]);
       }
     }
     fprintf(fp, "  }\n");
   }
 };
 std::vector<thread_conf> confs;
-void print_all_thread_conf();
+
+void print_all_thread_conf()
+{
+  printf("ALL CONFIGURATION \n");
+  size_t n_confs = confs.size();
+  for (size_t i=0; i<n_confs; i++) {
+    printf("conf[%zd]\n", i);
+    printf("{\n");
+    confs[i].dump(stdout);
+    printf("}\n");
+  }
+}
 
 bool is_power_of2(size_t num)
 {
@@ -100,16 +102,15 @@ bool is_power_of2(size_t num)
   return true;
 }
 
-void get_config(std::vector<thread_conf>& confs, size_t nb_cores, size_t nb_ques)
+void get_config(std::vector<thread_conf>& confs, size_t nb_cores, size_t nb_ports, size_t nb_ques)
 {
-  // nb_cores = 3;
-
   if (!is_power_of2(nb_cores))
     throw slankdev::exception("nb_lcore is not support");
 
+  block = true;
+
   confs.clear();
   confs.resize(nb_cores);
-  const size_t nb_ports = 2;
   const size_t nb_ques_per_core = nb_ques / nb_cores;
 
   size_t que_idx = 0;
@@ -121,29 +122,16 @@ void get_config(std::vector<thread_conf>& confs, size_t nb_cores, size_t nb_ques
       for (size_t qid=0; qid<nb_ques_per_core; qid++) {
         p.ques.push_back(qid+que_idx);
       }
-      confs[i].rx_port_range.push_back(p);
-      confs[i].tx_port_range.push_back(p);
+      confs[i].ports.push_back(p);
     }
     que_idx += nb_ques_per_core;
   }
-}
-
-void print_all_thread_conf()
-{
-  printf("ALL CONFIGURATION \n");
-  size_t n_confs = confs.size();
-  for (size_t i=0; i<n_confs; i++) {
-    printf("conf[%zd]\n", i);
-    printf("{\n");
-    confs[i].dump(stdout);
-    printf("}\n");
-  }
+  block = false;
 }
 
 void imple(void* arg)
 {
   size_t lid = *reinterpret_cast<size_t*>(arg);
-  thread_conf* conf = &(confs[lid]);
 
   printf("\n----------------------------\n");
   ssn_log(SSN_LOG_INFO, "start new thread \n");
@@ -152,23 +140,27 @@ void imple(void* arg)
 
   size_t nb_ports = ssn_dev_count();
   while (true) {
-#if 1
-    for (size_t pid=0; pid<nb_ports; pid++) {
-      printf("thread polling rxq=\n");
-      ssn_sleep(1000);
+    if (block) {
+      printf("block\n");
+      continue;
     }
-#else
+
     for (size_t pid=0; pid<nb_ports; pid++) {
       rte_mbuf* mbufs[32];
-      size_t nb_recv = ssn_port_rx_burst(pid, 0, mbufs, 32);
-      size_t nb_recv = conf->port(pid, 0, mbufs, 32);
+      size_t nb_recv = confs[lid].ports[pid].rx_burst(mbufs, 32);
       if (nb_recv == 0) continue;
 
-      size_t nb_send = ssn_port_tx_burst(pid^1,0, mbufs, nb_recv);
+      for (size_t i=0; i<nb_recv; i++) {
+        printf("recv %zd:%zd hash=0x%x\n", pid,
+            confs[lid].ports[pid].rxques_idx,
+            mbufs[i]->hash.rss);
+        // dpdk::hexdump_mbuf(stdout, mbufs[i]);
+      }
+
+      size_t nb_send = confs[lid].ports[pid^1].tx_burst(mbufs, nb_recv);
       if (nb_send < nb_recv)
         ssn_mbuf_free_bulk(&mbufs[nb_send], nb_recv-nb_send);
     }
-#endif
     ssn_yield();
   }
   ssn_log(SSN_LOG_INFO, "finish rx-thread \n");
@@ -176,106 +168,57 @@ void imple(void* arg)
 
 int main(int argc, char** argv)
 {
+  constexpr size_t nb_queues_per_port = 4;
+
   /*-BOOT-BEGIN--------------------------------------------------------------*/
-  ssn_log_set_level(SSN_LOG_DEBUG);
+  ssn_log_set_level(SSN_LOG_EMERG);
   ssn_init(argc, argv);
   ssn_green_thread_sched_register(1);
 
-  size_t nb_ports = ssn_dev_count();
+  const size_t nb_ports = ssn_dev_count();
   if (nb_ports != 2) throw slankdev::exception("num ports is not 2");
+
+  ssn_port_conf conf;
+  conf.nb_rxq = nb_queues_per_port;
+  conf.nb_txq = nb_queues_per_port;
+  conf.raw.rxmode.mq_mode = ETH_MQ_RX_RSS;
+  conf.raw.rx_adv_conf.rss_conf.rss_key = NULL;
+  conf.raw.rx_adv_conf.rss_conf.rss_hf = ETH_RSS_IP|ETH_RSS_TCP|ETH_RSS_UDP;
+  conf.debug_dump(stdout);
+  for (size_t i=0; i<nb_ports; i++) {
+    ssn_port_configure(i, &conf);
+    ssn_port_dev_up(i);
+    ssn_port_promisc_on(i);
+  }
   /*-BOOT-END----------------------------------------------------------------*/
 
+  /* 1 thread */
+  getchar();
+  get_config(confs, 1, nb_ports, nb_queues_per_port);
+  ssn_green_thread_launch(imple, &num0, 1);
+
+  /* 2 thread */
+  getchar();
+  get_config(confs, 2, nb_ports, nb_queues_per_port);
+  ssn_green_thread_launch(imple, &num1, 1);
+
+  /* 4 thread */
+  getchar();
+  get_config(confs, 4, nb_ports, nb_queues_per_port);
+  ssn_green_thread_launch(imple, &num2, 1);
+  ssn_green_thread_launch(imple, &num3, 1);
+
 #if 0
-  port p;
-
-  /*
-   * Num of Threads is 1
-   */
+  /* 8 thread */
   getchar();
-  confs.resize(1);
-
-  confs[0].clear();
-  confs[0].lcore_id = 0;
-  p.pid = 0; p.ques = {0,1,2,3}; confs[0].rx_port_range.push_back(p);
-  p.pid = 1; p.ques = {0,1,2,3}; confs[0].rx_port_range.push_back(p);
-  p.pid = 0; p.ques = {0,1,2,3}; confs[0].tx_port_range.push_back(p);
-  p.pid = 1; p.ques = {0,1,2,3}; confs[0].tx_port_range.push_back(p);
-
-  ssn_green_thread_launch(imple, &num0, 1);
-
-  /*
-   * Num of Threads is 2
-   */
-  getchar();
-  confs.resize(2);
-
-  confs[0].clear();
-  confs[0].lcore_id = 0;
-  p.pid = 0; p.ques = {0,1};     confs[0].rx_port_range.push_back(p);
-  p.pid = 1; p.ques = {0,1};     confs[0].rx_port_range.push_back(p);
-  p.pid = 0; p.ques = {0,1};     confs[0].tx_port_range.push_back(p);
-  p.pid = 1; p.ques = {0,1};     confs[0].tx_port_range.push_back(p);
-
-  confs[1].clear();
-  confs[1].lcore_id = 1;
-  p.pid = 0; p.ques = {2,3};     confs[1].rx_port_range.push_back(p);
-  p.pid = 1; p.ques = {2,3};     confs[1].rx_port_range.push_back(p);
-  p.pid = 0; p.ques = {2,3};     confs[1].tx_port_range.push_back(p);
-  p.pid = 1; p.ques = {2,3};     confs[1].tx_port_range.push_back(p);
-
-  ssn_green_thread_launch(imple, &num1, 1);
-
-  /*
-   * Num of Threads is 4 (LAST)
-   */
-  getchar();
-  confs.resize(4);
-
-  confs[0].clear();
-  confs[0].lcore_id = 0;
-  p.pid = 0; p.ques = {0};     confs[0].rx_port_range.push_back(p);
-  p.pid = 1; p.ques = {0};     confs[0].rx_port_range.push_back(p);
-  p.pid = 0; p.ques = {0};     confs[0].tx_port_range.push_back(p);
-  p.pid = 1; p.ques = {0};     confs[0].tx_port_range.push_back(p);
-
-  confs[1].clear();
-  confs[1].lcore_id = 1;
-  p.pid = 0; p.ques = {1};     confs[1].rx_port_range.push_back(p);
-  p.pid = 1; p.ques = {1};     confs[1].rx_port_range.push_back(p);
-  p.pid = 0; p.ques = {1};     confs[1].tx_port_range.push_back(p);
-  p.pid = 1; p.ques = {1};     confs[1].tx_port_range.push_back(p);
-
-  confs[2].clear();
-  confs[2].lcore_id = 2;
-  p.pid = 0; p.ques = {2};     confs[2].rx_port_range.push_back(p);
-  p.pid = 1; p.ques = {2};     confs[2].rx_port_range.push_back(p);
-  p.pid = 0; p.ques = {2};     confs[2].tx_port_range.push_back(p);
-  p.pid = 1; p.ques = {2};     confs[2].tx_port_range.push_back(p);
-
-  confs[3].clear();
-  confs[3].lcore_id = 3;
-  p.pid = 0; p.ques = {3};     confs[3].rx_port_range.push_back(p);
-  p.pid = 1; p.ques = {3};     confs[3].rx_port_range.push_back(p);
-  p.pid = 0; p.ques = {3};     confs[3].tx_port_range.push_back(p);
-  p.pid = 1; p.ques = {3};     confs[3].tx_port_range.push_back(p);
-
-  ssn_green_thread_launch(imple, &num2, 1);
-  ssn_green_thread_launch(imple, &num3, 1);
-#else
-  getchar();
-  get_config(confs, 1,4);
-  ssn_green_thread_launch(imple, &num0, 1);
-
-  getchar();
-  get_config(confs, 2,4);
-  ssn_green_thread_launch(imple, &num1, 1);
-
-  getchar();
-  get_config(confs, 4,4);
-  ssn_green_thread_launch(imple, &num2, 1);
-  ssn_green_thread_launch(imple, &num3, 1);
+  get_config(confs, 8,8);
+  ssn_green_thread_launch(imple, &num4, 1);
+  ssn_green_thread_launch(imple, &num5, 1);
+  ssn_green_thread_launch(imple, &num6, 1);
+  ssn_green_thread_launch(imple, &num7, 1);
 #endif
 
+  getchar();
 
   /*-FINI-BEGIN--------------------------------------------------------------*/
   ssn_green_thread_sched_unregister(1);
