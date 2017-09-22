9f3f3e4d8020598ad0fd97dd420adad67346271e
Hiroki SHIROKURA <slank.dev@gmail.com>
2017-09-22 23:15:12 +0900

    WIP Refact ssn_vnf prototype

--- examples/ssn_vnf/main.cc
+++ examples/ssn_vnf/main.cc
@@ -101,7 +101,7 @@ void get_config(std::vector<thread_conf>& confs, size_t nb_cores, size_t nb_port
 }
 
 bool running = true;
-void imple(void* arg)
+void vnf_imple(void* arg)
 {
   ssn_log(SSN_LOG_INFO, "start new thread \n");
   size_t lid = *reinterpret_cast<size_t*>(arg);
@@ -130,20 +130,152 @@ void imple(void* arg)
   ssn_log(SSN_LOG_INFO, "finish rx-thread \n");
 }
 
+enum ssn_vnf_port_type {
+  DPDK,
+  VIRT,
+};
+const char* ssn_vnf_port_type2str(enum ssn_vnf_port_type e)
+{
+  switch (e) {
+    case DPDK: return "DPDK";
+    case VIRT: return "VIRT";
+  }
+}
+class ssn_vnf_port {
+  const size_t port_id;
+  const size_t n_rx_queues;
+  const size_t n_tx_queues;
+  const ssn_vnf_port_type type;
+ public:
+  ssn_vnf_port(size_t pid, size_t ntxq, size_t nrxq) :
+    port_id(pid),
+    n_rx_queues(nrxq),
+    n_tx_queues(ntxq), type(DPDK) {}
+  void debug_dump(FILE* fp) const
+  {
+    /*             pid    nrxq   ntxq   type       */
+    fprintf(fp, "  %-3zd  %-4zd  %-4zd  %-10s \r\n",
+        port_id, n_rx_queues, n_tx_queues,
+        ssn_vnf_port_type2str(type));
+  }
+};
+
+
+class ssn_vnf;
+
+class ssn_nfvi {
+ public:
+  constexpr static size_t master_thread_lcore_mask = 0x01;
+  constexpr static size_t green_thread_lcore_mask  = 0x02;
+  constexpr static size_t timer_thread_lcore_mask  = 0x04;
+  constexpr static size_t native_thread_lcore_mask = 0xf8;
+  constexpr static size_t n_rx_queues_per_port = 4;
+  constexpr static size_t n_tx_queues_per_port = 4;
+  std::vector<ssn_vnf_port> ports;
+  std::vector<ssn_vnf*> vnfs;
+
+  void INIT(int argc, char** argv)
+  {
+    ssn_init(argc, argv);
+
+    std::vector<size_t> vec = coremask2vecor(green_thread_lcore_mask);
+    auto n_lcores = vec.size();
+    for (size_t i=0; i<n_lcores; i++) {
+      ssn_green_thread_sched_register(vec[i]);
+    }
+
+    ssn_port_conf conf;
+    conf.nb_rxq = n_rx_queues_per_port;
+    conf.nb_txq = n_tx_queues_per_port;
+    conf.raw.rxmode.mq_mode = ETH_MQ_RX_RSS;
+    conf.raw.rx_adv_conf.rss_conf.rss_key = NULL;
+    conf.raw.rx_adv_conf.rss_conf.rss_hf = ETH_RSS_IP|ETH_RSS_TCP|ETH_RSS_UDP;
+    /* conf.debug_dump(stdout); */
+
+    size_t n_ports = ssn_dev_count();
+    if (n_ports != 2) throw slankdev::exception("num ports is not 2");
+    for (size_t i=0; i<n_ports; i++) {
+      ssn_port_configure(i, &conf);
+      ssn_port_dev_up(i);
+      ssn_port_promisc_on(i);
+
+      ssn_vnf_port port(i, n_tx_queues_per_port, n_rx_queues_per_port);
+      ports.push_back(port);
+    }
+
+    if (n_ports != 2) {
+      std::string err = slankdev::format("num ports is not 2 (current %zd)",
+          ssn_dev_count());
+      throw slankdev::exception(err.c_str());
+    }
+  }
+  void FINI()
+  {
+    std::vector<size_t> vec = coremask2vecor(green_thread_lcore_mask);
+    auto n_lcores = vec.size();
+    for (size_t i=0; i<n_lcores; i++) {
+      ssn_green_thread_sched_unregister(vec[i]);
+    }
+    ssn_wait_all_lcore();
+    ssn_fin();
+  }
+  void ports_debug_dump(FILE* fp)
+  {
+    fprintf(fp, "\r\n");
+    fprintf(fp, "  %-3s  %-4s  %-4s  %-10s \r\n", "pid", "nrxq", "ntxq", "type");
+    fprintf(fp, " -----------------------------------------------------\r\n");
+    size_t n_ports = ports.size();
+    for (size_t i=0; i<n_ports; i++) {
+      ports[i].debug_dump(fp);
+    }
+    fprintf(fp, "\r\n");
+  }
+  void vnfs_debug_dump(FILE* fp);
+};
+
 class ssn_vnf {
   const size_t n_ports;
   const size_t n_rx_queues_per_port;
   const size_t n_tx_queues_per_port;
   size_t n_threads;
   size_t core_mask;
+  size_t port_mask;
   std::vector<uint32_t> tids;
+  ssn_nfvi* nfvi;
+  ssn_function_t imple;
 
  public:
-  ssn_vnf(size_t np, size_t ntxq, size_t nrxq) :
-    n_ports(np),
-    n_rx_queues_per_port(nrxq),
-    n_tx_queues_per_port(ntxq) {}
-
+  ssn_vnf(ssn_nfvi* nfvi_, size_t n_threads_,
+      uint32_t core_mask_, uint32_t port_mask_,
+      ssn_function_t f) :
+    n_ports(nfvi_->ports.size()),
+    n_rx_queues_per_port(nfvi_->n_rx_queues_per_port),
+    n_tx_queues_per_port(nfvi_->n_tx_queues_per_port),
+    n_threads(n_threads_),
+    core_mask(core_mask_),
+    port_mask(port_mask_),
+    nfvi(nfvi_), imple(f)
+  {
+    nfvi->vnfs.push_back(this);
+    get_config(confs, n_threads, n_ports, n_rx_queues_per_port, n_tx_queues_per_port);
+  }
+  ~ssn_vnf()
+  {
+    size_t n_vnfs = nfvi->vnfs.size();
+    for (size_t i=0; i<n_vnfs; i++) {
+      if (nfvi->vnfs[i] == this) {
+        nfvi->vnfs.erase(nfvi->vnfs.begin() + i);
+        return ;
+      }
+    }
+    throw slankdev::exception("OKASHIII");
+  }
+  void debug_dump(FILE* fp) const
+  {
+    /*             nports coremask portmask nthreads */
+    fprintf(fp, "  %-5zd 0x%04lx 0x%04lx  %zd\r\n",
+        n_ports, core_mask, port_mask, n_threads);
+  }
   void undeploy()
   {
     running = false;
@@ -151,15 +283,6 @@ class ssn_vnf {
       ssn_thread_join(tids[i]);
     }
     printf("UNDEPLOY SUCCESS\n");
-  }
-  void reconf(size_t n_threads_, size_t core_mask_)
-  {
-    n_threads = n_threads_;
-    core_mask = core_mask_;
-    uint32_t tid0,tid1,tid2,tid3;
-
-    get_config(confs, n_threads, n_ports, n_rx_queues_per_port, n_tx_queues_per_port);
-    printf("RECONF SUCCESS\n");
   }
   void showconf()
   {
@@ -186,86 +309,48 @@ class ssn_vnf {
   }
 };
 
-class ssn_nfvi {
- public:
-  constexpr static size_t master_thread_lcore_mask = 0x01;
-  constexpr static size_t green_thread_lcore_mask  = 0x02;
-  constexpr static size_t timer_thread_lcore_mask  = 0x04;
-  constexpr static size_t native_thread_lcore_mask = 0xf8;
-  constexpr static size_t n_rx_queues_per_port = 4;
-  constexpr static size_t n_tx_queues_per_port = 4;
-  size_t n_ports;
-
-  void INIT(int argc, char** argv)
-  {
-    ssn_init(argc, argv);
-
-    std::vector<size_t> vec = coremask2vecor(green_thread_lcore_mask);
-    auto n_lcores = vec.size();
-    for (size_t i=0; i<n_lcores; i++) {
-      ssn_green_thread_sched_register(vec[i]);
-    }
-
-    ssn_port_conf conf;
-    conf.nb_rxq = n_rx_queues_per_port;
-    conf.nb_txq = n_rx_queues_per_port;
-    conf.raw.rxmode.mq_mode = ETH_MQ_RX_RSS;
-    conf.raw.rx_adv_conf.rss_conf.rss_key = NULL;
-    conf.raw.rx_adv_conf.rss_conf.rss_hf = ETH_RSS_IP|ETH_RSS_TCP|ETH_RSS_UDP;
-    /* conf.debug_dump(stdout); */
-
-    n_ports = ssn_dev_count();
-    if (n_ports != 2) throw slankdev::exception("num ports is not 2");
-    for (size_t i=0; i<n_ports; i++) {
-      ssn_port_configure(i, &conf);
-      ssn_port_dev_up(i);
-      ssn_port_promisc_on(i);
-    }
-
-    if (n_ports != 2) {
-      std::string err = slankdev::format("num ports is not 2 (current %zd)",
-          ssn_dev_count());
-      throw slankdev::exception(err.c_str());
-    }
+void ssn_nfvi::vnfs_debug_dump(FILE* fp)
+{
+  fprintf(fp, "\r\n");
+    /*             nports coremask portmask nthreads */
+  fprintf(fp, "  %-5s %-6s %-6s  %s \r\n", "nport", "cmask", "pmask", "nthrd");
+  fprintf(fp, "---------------------------------------------------------\r\n");
+  size_t n_vnfs = vnfs.size();
+  for (auto* vnf : vnfs) {
+    vnf->debug_dump(stdout);
   }
-  void FINI()
-  {
-    std::vector<size_t> vec = coremask2vecor(green_thread_lcore_mask);
-    auto n_lcores = vec.size();
-    for (size_t i=0; i<n_lcores; i++) {
-      ssn_green_thread_sched_unregister(vec[i]);
-    }
-    ssn_wait_all_lcore();
-    ssn_fin();
-  }
-};
-
+  fprintf(fp, "\r\n");
+}
 
 int main(int argc, char** argv)
 {
   ssn_nfvi nfvi;
   nfvi.INIT(argc, argv);
+  ssn_vnf* vnf0 = nullptr;
 
-  ssn_vnf vnf0( nfvi.n_ports,
-      nfvi.n_tx_queues_per_port,
-      nfvi.n_rx_queues_per_port);
-
-  getchar();
-  vnf0.reconf(1, 0x04);
-  vnf0.showconf();
-  vnf0.deploy();
+  nfvi.ports_debug_dump(stdout);
 
   getchar();
-  vnf0.undeploy();
-  vnf0.reconf(2, 0x0c);
-  vnf0.showconf();
-  vnf0.deploy();
+  vnf0 = new ssn_vnf(&nfvi, 1, 0x04, 0x03, vnf_imple);
+  nfvi.vnfs_debug_dump(stdout);
+  vnf0->showconf();
+  vnf0->deploy();
 
   getchar();
-  vnf0.undeploy();
-  vnf0.reconf(4, 0x3c);
-  vnf0.showconf();
-  vnf0.deploy();
+  vnf0->undeploy();
+  delete vnf0;
+  vnf0 = new ssn_vnf(&nfvi, 2, 0x0c, 0x03, vnf_imple);
+  nfvi.vnfs_debug_dump(stdout);
+  vnf0->showconf();
+  vnf0->deploy();
+
+  getchar();
+  vnf0->undeploy();
+  delete vnf0;
+  vnf0 = new ssn_vnf(&nfvi, 4, 0x3c, 0x03, vnf_imple);
+  nfvi.vnfs_debug_dump(stdout);
+  vnf0->showconf();
+  vnf0->deploy();
 
   getchar();
   nfvi.FINI();
