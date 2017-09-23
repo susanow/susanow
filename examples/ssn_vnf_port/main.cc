3cd377eda63d353869178c75deefed6c9beb5348
Hiroki SHIROKURA <slank.dev@gmail.com>
2017-09-24 00:46:22 +0900

    WIP Refact: ssn_vnf_port example

--- examples/ssn_vnf_port/main.cc
+++ examples/ssn_vnf_port/main.cc
@@ -20,7 +20,6 @@
 
 /*-------------------User-Code-Below-------------------------*/
 
-ssn_vnf_port p[2];
 size_t num[] = {0,1,2,3,4,5,6,7,8};
 
 bool l2fwd_running = true;
@@ -33,7 +32,7 @@ void l2fwd(void* acc_id_)
   while (l2fwd_running) {
     for (size_t pid=0; pid<nb_ports; pid++) {
       rte_mbuf* mbufs[32];
-      size_t nb_recv = p[pid].rx_burst(aid, mbufs, 32);
+      size_t nb_recv = ssn_vnf_port_rx_burst(pid, aid, mbufs, 32);
       if (nb_recv == 0) continue;
 
       for (size_t i=0; i<nb_recv; i++) {
@@ -42,7 +41,7 @@ void l2fwd(void* acc_id_)
         size_t n=10;
         for (size_t j=0; j<100; j++) n++;
 
-        size_t nb_send = p[pid^1].tx_burst(aid, &mbufs[i], 1);
+        size_t nb_send = ssn_vnf_port_tx_burst(pid^1, aid, &mbufs[i], 1);
         if (nb_send != 1)
           rte_pktmbuf_free(mbufs[i]);
       }
@@ -59,9 +58,9 @@ void INIT(int argc, char** argv, size_t n_que)
   size_t n_ports = ssn_dev_count();
   if (n_ports != 2) throw slankdev::exception("num ports is not 2");
   for (size_t i=0; i<n_ports; i++) {
-    p[i].configure_hwqueue(i,n_que,n_que);
-    p[i].dev_up();
-    p[i].promisc_on();
+    ssn_vnf_port_configure_hw(i, n_que, n_que);
+    ssn_vnf_port_dev_up(i);
+    ssn_vnf_port_promisc_on(i);
   }
 
   if (n_ports != 2) {
@@ -77,16 +76,16 @@ int main(int argc, char** argv)
   INIT(argc, argv, 4);
 
   getchar();
-  p[0].configure_accessor(1, 1);
-  p[1].configure_accessor(1, 1);
+  ssn_vnf_port_configure_acc(0, 1, 1);
+  ssn_vnf_port_configure_acc(1, 1, 1);
   l2fwd_running = true;
   tid[0] = ssn_thread_launch(l2fwd, &num[0], 1);
 
   getchar();
   l2fwd_running = false;
   ssn_thread_join(tid[0]);
-  p[0].configure_accessor(2, 2);
-  p[1].configure_accessor(2, 2);
+  ssn_vnf_port_configure_acc(0, 2, 2);
+  ssn_vnf_port_configure_acc(1, 2, 2);
   l2fwd_running = true;
   tid[0] = ssn_thread_launch(l2fwd, &num[0], 1);
   tid[1] = ssn_thread_launch(l2fwd, &num[1], 2);
@@ -95,8 +94,8 @@ int main(int argc, char** argv)
   l2fwd_running = false;
   ssn_thread_join(tid[0]);
   ssn_thread_join(tid[1]);
-  p[0].configure_accessor(4, 4);
-  p[1].configure_accessor(4, 4);
+  ssn_vnf_port_configure_acc(0, 4, 4);
+  ssn_vnf_port_configure_acc(1, 4, 4);
   l2fwd_running = true;
   tid[0] = ssn_thread_launch(l2fwd, &num[0], 1);
   tid[1] = ssn_thread_launch(l2fwd, &num[1], 2);
--- examples/ssn_vnf_port/ssn_vnf_port.cc
+++ examples/ssn_vnf_port/ssn_vnf_port.cc
@@ -4,6 +4,95 @@
 #include <slankdev/exception.h>
 
 
+/*****************************************************************************\
+ * Class Definition are Below...
+\*****************************************************************************/
+
+class ssn_vnf_port {
+ private:
+
+  class ssn_vnf_port_oneside {
+   private:
+    class accessor {
+      size_t accessor_idx;
+      std::vector<size_t> ques;
+     public:
+      accessor() {}
+      void set(std::vector<size_t>& vec);
+      size_t get();
+    };
+    class ssn_vnf_port_queue {
+     public:
+      size_t que_id;
+      size_t acc_id;
+    };
+   protected:
+    size_t dpdk_port_id;
+    size_t n_queues_;
+    size_t n_accessor;
+    std::vector<ssn_vnf_port_queue> queues;
+    std::vector<accessor> accessors;
+   protected:
+    std::vector<size_t> get_qids_from_aid(size_t aid) const;
+    size_t n_queues_per_accessor() const;
+    void show() const;
+   public:
+    ssn_vnf_port_oneside() : n_queues_(1), n_accessor(1), accessors(1) {}
+    virtual size_t burst(size_t aid, rte_mbuf** mbufs, size_t n_mbufs) = 0;
+    size_t n_queues() const { return n_queues_; }
+    void configure_queue_accessor(size_t dpdk_pid, size_t n_que, size_t n_acc);
+  };
+  class ssn_vnf_port_oneside_rx : public ssn_vnf_port_oneside {
+   public:
+    ssn_vnf_port_oneside_rx() : ssn_vnf_port_oneside() {}
+    virtual size_t burst(size_t aid, rte_mbuf** mbufs, size_t n_mbufs) override
+    {
+      size_t qid = accessors[aid].get();
+      ssn_log(SSN_LOG_DEBUG,
+          "ssn_vnf_port: rx_burst(pid=%zd, qid=%zd, access_id=%zd)",
+          dpdk_port_id, qid, aid);
+      return rte_eth_rx_burst(dpdk_port_id, qid, mbufs, n_mbufs);
+    }
+  };
+  class ssn_vnf_port_oneside_tx : public ssn_vnf_port_oneside {
+   public:
+    ssn_vnf_port_oneside_tx() : ssn_vnf_port_oneside() {}
+    virtual size_t burst(size_t aid, rte_mbuf** mbufs, size_t n_mbufs) override
+    {
+      size_t qid = accessors[aid].get();
+      ssn_log(SSN_LOG_DEBUG,
+          "ssn_vnf_port: tx_burst(pid=%zd, qid=%zd, access_id=%zd)",
+          dpdk_port_id, qid, aid);
+      return rte_eth_tx_burst(dpdk_port_id, qid, mbufs, n_mbufs);
+    }
+  };
+
+ private:
+  size_t dpdk_pid;
+  size_t n_rxq;
+  size_t n_txq;
+  ssn_vnf_port_oneside_rx rx;
+  ssn_vnf_port_oneside_tx tx;
+
+ public:
+  void configure_hwqueue(size_t dpdk_pid_, size_t n_rxq_i, size_t n_txq_i);
+  void configure_accessor(size_t n_rxacc, size_t n_txacc);
+  size_t rx_burst(size_t aid, rte_mbuf** mbufs, size_t nb_mbufs);
+  size_t tx_burst(size_t aid, rte_mbuf** mbufs, size_t nb_mbufs);
+  void link_up()       { ssn_port_link_up    (dpdk_pid); }
+  void link_down()     { ssn_port_link_down  (dpdk_pid); }
+  void promisc_on()  { ssn_port_promisc_on (dpdk_pid); }
+  void promisc_off() { ssn_port_promisc_off(dpdk_pid); }
+  void dev_up()      { ssn_port_dev_up     (dpdk_pid); }
+  void dev_down()    { ssn_port_dev_down   (dpdk_pid); }
+}; /* class ssn_vnf_port */
+
+
+
+/*****************************************************************************\
+ * Instance Function are Below...
+\*****************************************************************************/
+
 void ssn_vnf_port::ssn_vnf_port_oneside::accessor::set(std::vector<size_t>& vec)
 {
   ques = vec;
@@ -133,4 +222,53 @@ size_t ssn_vnf_port::tx_burst(size_t aid, rte_mbuf** mbufs, size_t nb_mbufs)
 { return tx.burst(aid, mbufs, nb_mbufs); }
 
 
+
+/*****************************************************************************\
+ * SSN_APIs are Below...
+\*****************************************************************************/
+
+/* SSN_VNF_PORT Instances */
+ssn_vnf_port vnf_ports[RTE_MAX_ETHPORTS];
+
+void ssn_vnf_port_link_up(size_t pid)     { ssn_port_link_up    (pid); }
+void ssn_vnf_port_link_down(size_t pid)   { ssn_port_link_down  (pid); }
+void ssn_vnf_port_promisc_on(size_t pid)  { ssn_port_promisc_on (pid); }
+void ssn_vnf_port_promisc_off(size_t pid) { ssn_port_promisc_off(pid); }
+void ssn_vnf_port_dev_up(size_t pid)      { ssn_port_dev_up     (pid); }
+void ssn_vnf_port_dev_down(size_t pid)    { ssn_port_dev_down   (pid); }
+
+void ssn_vnf_port_configure_hw(size_t port_id, size_t n_rxq, size_t n_txq)
+{
+  if (port_id >= ssn_dev_count()) {
+    std::string err = "ssn_vnf_port_configure_hw: ";
+    err += slankdev::format("port_id is invalid pid=%zd", port_id);
+    throw slankdev::exception(err.c_str());
+  }
+  vnf_ports[port_id].configure_hwqueue(port_id, n_rxq, n_txq);
+}
+
+void ssn_vnf_port_configure_acc(size_t port_id, size_t n_rxacc, size_t n_txacc)
+{
+  if (port_id >= ssn_dev_count()) {
+    std::string err = "ssn_vnf_port_configure_acc: ";
+    err += slankdev::format("port_id is invalid pid=%zd", port_id);
+    throw slankdev::exception(err.c_str());
+  }
+  vnf_ports[port_id].configure_accessor(n_rxacc, n_txacc);
+}
+
+size_t ssn_vnf_port_rx_burst(size_t port_id, size_t aid, rte_mbuf** mbufs, size_t n_mbufs)
+{
+  return vnf_ports[port_id].rx_burst(aid,  mbufs, n_mbufs);
+}
+
+size_t ssn_vnf_port_tx_burst(size_t port_id, size_t aid, rte_mbuf** mbufs, size_t n_mbufs)
+{
+  return vnf_ports[port_id].tx_burst(aid,  mbufs, n_mbufs);
+}
+
+void ssn_vnf_port_debug_dump(FILE* fp)
+{
+  fprintf(fp, " ssn_vnf_port_debug_dump \r\n");
+}
 
