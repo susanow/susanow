65066649212db568c34ca28855252956e9683288
Hiroki SHIROKURA <slank.dev@gmail.com>
2017-11-21 23:48:08 +0000

    restapi: refactor

--- webui/main.js
+++ webui/main.js
@@ -1,26 +1,22 @@
 
-$(function(){
 
-	$.getJSON('http://10.10.0.20:8888' , function(data){
+var update = function(){
+
+	var src;
+	src = 'data.json';
+	src = 'http://10.10.0.20:8888';
+	$.getJSON(src , function(data){
 		n_vnf  = data.n_vnf;
 		n_port = data.n_port;
 		n_vcat = data.n_vcat;
 		n_pcat = data.n_pcat;
-		document.write('n_vnf : ' + n_vnf  + '<br>');
-		document.write('n_port: ' + n_port + '<br>');
-		document.write('n_vcat: ' + n_vcat + '<br>');
-		document.write('n_pcat: ' + n_pcat + '<br>');
 
-		for (var i=0; i<n_vnf; i++) {
-			document.write('vnf[' + i + '] <br>');
-		}
-
-		document.write('<br><br>');
+		document.getElementById("n_vnf" ).innerText = n_vnf;
+		document.getElementById("n_port").innerText = n_port;
+		document.getElementById("n_vcat").innerText = n_vcat;
+		document.getElementById("n_pcat").innerText = n_pcat;
 	});
 
-	$.getJSON('http://10.10.0.20:8888/vnfs/vnf0' , function(data){
-		document.write(JSON.stringify(data));
-	});
+}
 
-});
-
+setInterval(update, 1000);
