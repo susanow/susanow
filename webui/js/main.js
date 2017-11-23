232bb3773d52fd7134807cd04bae9c3a6e8ffb16
Hiroki SHIROKURA <slank.dev@gmail.com>
2017-11-23 18:12:06 +0000

    WIP webui: impl

--- webui/js/main.js
+++ webui/js/main.js
@@ -1,11 +1,73 @@
 
+const URL = 'http://10.10.0.20:8888';
 
-var update = function(){
+var already_registered = function(name, list){
+	n_ele = list.length;
+	for (var i=0; i<n_ele; i++) {
+		text = list[i].children[0].innerText;
+		if (text == name) {
+			return true;
+		}
+	}
+	return false;
+}
 
-	var src;
-	src = 'data.json';
-	src = 'http://10.10.0.20:8888';
-	$.getJSON(src , function(data){
+var update_vnf = function(){
+	$.getJSON(URL + '/vnfs' , function(data){
+
+		var n_vnf_ = data.n_vnf;
+		for (var i=0; i<n_vnf_; i++) {
+			var vnf = data[i];
+			var name = vnf.name;
+
+			list = $('#vnf-list').find('li');
+			if (!already_registered(name, list)) {
+				var list = document.getElementById("vnf-list");
+				var str =
+					'<li>' +
+					'	<div id="vnf-name" class="collapsible-header">' +
+					'		NAME' +
+					'	</div>' +
+					'	<div class="collapsible-body">' +
+					'		<p>Lorem ipsum dolor sit amet.</p>' +
+					'	</div>' +
+					'</li>';
+				str = str.replace('NAME', name);
+				$(str).appendTo(list);
+			}
+		}
+
+	});
+}
+
+var update_port = function(){
+	$.getJSON(URL + '/ports' , function(data){
+		var n_ports = data.n_port;
+		for (var i=0; i<n_port; i++) {
+			var port = data[i];
+			var name = port.name;
+
+			list = $('#port-list').find('li');
+			if (!already_registered(name, list)) {
+				var list = document.getElementById("port-list");
+				var str =
+					'<li>' +
+					'	<div class="collapsible-header">' +
+					'		NAME' +
+					'	</div>' +
+					'	<div class="collapsible-body">' +
+					'		<p>Lorem ipsum dolor sit amet.</p>' +
+					'	</div>' +
+					'</li>';
+				str = str.replace('NAME', name);
+				$(str).appendTo(list);
+			}
+		}
+	});
+}
+
+var update_system = function(){
+	$.getJSON(URL , function(data){
 		n_vnf  = data.n_vnf;
 		n_port = data.n_port;
 		n_vcat = data.n_vcat;
@@ -16,7 +78,13 @@ var update = function(){
 		document.getElementById("n_vcat").innerText = n_vcat;
 		document.getElementById("n_pcat").innerText = n_pcat;
 	});
+}
 
+var update = function(){
+	update_system();
+	update_vnf();
+	update_port();
 }
 
 setInterval(update, 1000);
+
