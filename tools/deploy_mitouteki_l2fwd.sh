70d8b2546c53b090fc1ccbe2c5d1bd4961beffd7
Hiroki SHIROKURA <slank.dev@gmail.com>
2017-12-04 14:55:43 +0900

    misc: refactor

--- tools/deploy_mitouteki_l2fwd.sh
+++ tools/deploy_mitouteki_l2fwd.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ssnctl vnf vnf0 alloc l2fwd1b
-ssnctl port pci0  alloc pci 0000:01:00.0
-ssnctl port pci1  alloc pci 0000:01:00.1
+ssnctl port pci0  alloc pci 0000:3b:00.1
+ssnctl port pci1  alloc pci 0000:5e:00.1
 ssnctl vnf vnf0 attachport 0 pci0
 ssnctl vnf vnf0 attachport 1 pci1
 
